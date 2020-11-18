ffdab6ad92c3213ff32f0955786ff7b3e731ce45
Oscar Wilhelmsson <karloscarwilhelmsson@gmail.com>
2020-11-18 16:36:48 +0100

    Added script to make player stick to "rotator"

--- Misfortune_Tadpole/Assets/Scripts/RotatorRotation.cs
+++ Misfortune_Tadpole/Assets/Scripts/RotatorRotation.cs
@@ -4,9 +4,41 @@ using UnityEngine;
 
 public class RotatorRotation : MonoBehaviour
 {
+    Rigidbody2D playerRb2d;
+    Rigidbody2D rotatorRb2d;
+    float rotationSpeed = 0;
+
+
+    private void Start()
+    {
+        rotatorRb2d = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+      /*  rotationSpeed = rotationSpeed + 0.00000000001f;
+        rotatorRb2d.MoveRotation(rotationSpeed);
+
+        if (rotationSpeed == 360)
+        {
+            rotationSpeed = 0;
+        }*/
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.parent = transform;
+            playerRb2d = collision.gameObject.GetComponent<Rigidbody2D>();
+            playerRb2d.isKinematic = true;
+            playerRb2d.gravityScale = 0;
+        }
     }
 }

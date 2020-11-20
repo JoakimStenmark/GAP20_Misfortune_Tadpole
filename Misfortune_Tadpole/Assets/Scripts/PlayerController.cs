ec2e611cc5fcf8bbbeebf6bf0df980ae287a7e24
Axel Eriksson <a.eriksson0303@gmail.com>
2020-11-20 11:55:26 +0100

    Added MainMenu scene, changed build settings index of scenes

--- Misfortune_Tadpole/Assets/Scripts/PlayerController.cs
+++ Misfortune_Tadpole/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -24,6 +25,10 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
 
         if (Input.GetKeyDown("r"))
         {

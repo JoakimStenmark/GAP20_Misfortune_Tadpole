c9461d537666e8448d92d8a24a7448d4d254868e
Axel Eriksson <a.eriksson0303@gmail.com>
2020-11-23 11:16:55 +0100

    Added PauseMenu to UI and added UI as canvas prefab

---
+++ Misfortune_Tadpole/Assets/PauseMenuManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+	private bool gameIsPaused;
+	private bool mouseIsVisible;
+
+	public GameObject PauseMenuUI;
+
+	private void Start()
+	{
+		mouseIsVisible = true;
+		gameIsPaused = false;
+		
+		ToggleMouseVisibility();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			TogglePauseState();
+		}
+	}
+
+	public void TogglePauseState()
+	{
+		gameIsPaused = !gameIsPaused;
+		if (gameIsPaused)
+		{
+			PauseMenuUI.SetActive(true);
+			Time.timeScale = 0f;
+			ToggleMouseVisibility();
+		}
+		else if (!gameIsPaused)
+		{
+			PauseMenuUI.SetActive(false);
+			Time.timeScale = 1f;
+			ToggleMouseVisibility();
+		}
+	}
+
+	private void ToggleMouseVisibility()
+	{
+		if (mouseIsVisible)
+		{
+			Cursor.visible = false;
+			mouseIsVisible = false;
+		}
+		else
+		{
+			Cursor.visible = true;
+			mouseIsVisible = true;
+		}
+	}
+
+	public void GoToMainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+}

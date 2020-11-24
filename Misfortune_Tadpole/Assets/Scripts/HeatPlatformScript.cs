43d02cb8045f5be0b128569af58e4e2aa2df8ab7
Axel Eriksson <a.eriksson0303@gmail.com>
2020-11-24 14:24:45 +0100

    Heat Platform

---
+++ Misfortune_Tadpole/Assets/Scripts/HeatPlatformScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatPlatformScript : MonoBehaviour
+{
+	public int heatPlatformDmgPerSec;
+
+	public PlayerController playerController;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			playerController = collision.gameObject.GetComponent<PlayerController>();
+			playerController.ChangeWaterAmount(heatPlatformDmgPerSec);
+		}
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			playerController = collision.gameObject.GetComponent<PlayerController>();
+			playerController.ChangeWaterAmount(heatPlatformDmgPerSec);
+		}
+	}
+}

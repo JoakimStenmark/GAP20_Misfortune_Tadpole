92c5e38eabcdfb1c5d3f96ce8bee0baf80ff858b
Oscar Wilhelmsson <karloscarwilhelmsson@gmail.com>
2020-11-24 14:16:07 +0100

    Added Spike prefab and script

---
+++ Misfortune_Tadpole/Assets/Scripts/SpikeScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeScript : MonoBehaviour
+{
+    public PlayerController playerController;
+    public int spikeDmg = -10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerController = collision.gameObject.GetComponent<PlayerController>();
+            playerController.ChangeWaterAmount(spikeDmg);
+        }
+    }
+}

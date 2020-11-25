d391953320dd30d000cf23a3aa6ffb6b7f0ec2de
Joakim Stenmark <jocke.stenmark@live.se>
2020-11-25 10:41:01 +0100

    Water Drop Pick up

---
+++ Misfortune_Tadpole/Assets/Scripts/WaterDrop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDrop : MonoBehaviour
+{
+    public int waterToAdd;
+
+    private void Start()
+    {
+        waterToAdd = Mathf.Abs(waterToAdd);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {      
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            other.gameObject.GetComponent<PlayerController>().ChangeWaterAmount(waterToAdd);
+        }
+    }
+
+}

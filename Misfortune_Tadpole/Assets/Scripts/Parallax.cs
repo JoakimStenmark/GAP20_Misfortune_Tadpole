6fccfe6c3bed8e03fba672e9ce7a3dc8661480a8
Joakim Stenmark <jocke.stenmark@live.se>
2020-11-25 15:18:50 +0100

    Basic parallax code done

---
+++ Misfortune_Tadpole/Assets/Scripts/Parallax.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float startPosX, startPosY;
+    private float length;
+    public GameObject mainCamera;
+    [Range(0.0f, 1.0f)] public float parallax;
+
+
+    void Start()
+    {
+        startPosX = transform.position.x;
+        startPosY = transform.position.y;
+
+    }
+
+    void FixedUpdate()
+    {
+        float distX = mainCamera.transform.position.x * parallax;
+        float distY = mainCamera.transform.position.y * parallax;
+
+        transform.position = new Vector3(startPosX + distX, startPosY + distY, transform.position.z);
+    }
+}

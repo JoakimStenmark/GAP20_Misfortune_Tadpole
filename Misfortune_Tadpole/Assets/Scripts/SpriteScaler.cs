4d4d464a984440999fac8208ef3f06ebccb68a9b
Axel Eriksson <a.eriksson0303@gmail.com>
2020-12-03 11:25:22 +0100

    Added HeatPlatform functionality to prefab and changes to sprite scaler script

--- Misfortune_Tadpole/Assets/Scripts/SpriteScaler.cs
+++ Misfortune_Tadpole/Assets/Scripts/SpriteScaler.cs
@@ -7,20 +7,19 @@ using Random = UnityEngine.Random;
 
 public class SpriteScaler : MonoBehaviour
 {
-    public float impactSquishX = 1.5f;
-    public float impactSquishY = 0.8f;
-    public float impactYOffset;
     public float circleCastRadius = 1;
     public bool hasHit;
 
+    public Vector2 defaultScale = new Vector2(1, 1);
+    public Vector2 squishScale = new Vector2(1.2f, 0.8f);
+
     public LayerMask mask;
-    // Start is called before the first frame update
+
     void Start()
     {
-        impactYOffset = 0.05f;
-    }
 
-    // Update is called once per frame
+    }
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
@@ -28,45 +27,39 @@ public class SpriteScaler : MonoBehaviour
             //transform.localScale = transform.localScale * Random.value;
             ImpactSquish();
         }
-        SetDefaultScale();
     }
 
     private void FixedUpdate()
     {
         RaycastHit2D hit = Physics2D.CircleCast(transform.position, circleCastRadius, Vector2.zero, 0f, mask);
-
-        if (hit.collider && !hit.collider.isTrigger)
+        
+        if (hit.collider && !hit.collider.isTrigger && !hasHit)
         {
             hasHit = true;
-        }
-        else
-        {
-            hasHit = false;
-        }
-
-        if (hasHit)
-        {
             ImpactSquish();
-            hasHit = false;
+            Invoke(nameof(SetDefaultScale), 0.5f);
         }
+        else if(!hit.collider)
+        {
+            hasHit = false;
+            SetDefaultScale();
+        }
+        
+        
     }
 
     private void SetDefaultScale()
     {
-        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, Time.deltaTime *2);
+        transform.localScale = Vector3.Lerp(transform.localScale, defaultScale, Time.deltaTime * 10);
     }
 
     private void ImpactSquish()
     {
-        Vector3 squish = new Vector3(impactSquishX, impactSquishY,1f);
-        Vector2 positionOffset = new Vector2(transform.localPosition.x, transform.localPosition.y - impactYOffset);
-        transform.localScale = Vector3.Lerp(squish, Vector3.one, Time.deltaTime);
-       // transform.localPosition = positionOffset;
-
+        transform.localScale = Vector3.Lerp(transform.localScale, squishScale, Time.deltaTime * 10);
     }
 
     private void OnDrawGizmos()
     {
-        
+        Gizmos.DrawWireSphere(transform.position, circleCastRadius);
     }
 }

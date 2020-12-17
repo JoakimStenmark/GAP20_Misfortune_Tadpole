6f9bafce92ed9f41b17f7d5532620393037caffd
Joakim Stenmark <jocke.stenmark@live.se>
2020-12-17 15:50:40 +0100

    Testplatform friction 0.3

---
+++ Misfortune_Tadpole/Assets/Scripts/ShowCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShowCollider : MonoBehaviour
+{
+
+    #if UNITY_EDITOR
+    void OnDrawGizmos() 
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, transform.lossyScale);
+    }
+
+    #endif
+}

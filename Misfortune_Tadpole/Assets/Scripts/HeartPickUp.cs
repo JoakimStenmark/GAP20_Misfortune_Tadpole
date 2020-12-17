b6c1e76e73c1c5a26290411855530fa9c34d24e1
Oscar Wilhelmsson <karloscarwilhelmsson@gmail.com>
2020-12-17 14:57:04 +0100

    Bugfix: Player getting two hearts when pikcing up one.

--- Misfortune_Tadpole/Assets/Scripts/HeartPickUp.cs
+++ Misfortune_Tadpole/Assets/Scripts/HeartPickUp.cs
@@ -2,10 +2,13 @@
 
 public class HeartPickUp : MonoBehaviour
 {
+    private bool gainedLife;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !gainedLife)
         {
+            gainedLife = true;
             other.gameObject.GetComponent<PlayerController>().ChangeLifeAmount(true);
             gameObject.SetActive(false);
         }

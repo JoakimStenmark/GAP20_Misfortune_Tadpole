3a74034b1d1397e9fcf355925e390c61e3a70470
JoakimStenmark <70517100+JoakimStenmark@users.noreply.github.com>
2020-12-21 16:56:13 +0100

    Working volume sliders

---
+++ Misfortune_Tadpole/Assets/Scripts/AudioOptions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioOptions : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider soundSlider;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = Mathf.Lerp(-80, 0, musicSlider.value * 0.01f);
+        Debug.Log(volume);
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+    public void SetSoundVolume()
+    {
+        float volume = Mathf.Lerp(-80, 0, soundSlider.value * 0.01f);
+
+        audioMixer.SetFloat("SoundVolume", volume);
+    }
+}

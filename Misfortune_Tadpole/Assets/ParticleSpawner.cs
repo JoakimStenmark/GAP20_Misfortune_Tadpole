d75ddee85f7ebd9635e3abea140b3eb1c652622a
JoakimStenmark <70517100+JoakimStenmark@users.noreply.github.com>
2020-12-28 13:39:28 +0100

    Wind animation thing, Snowflakes on IcePlatforms, .exe icon set

--- Misfortune_Tadpole/Assets/ParticleSpawner.cs
+++ Misfortune_Tadpole/Assets/ParticleSpawner.cs
@@ -7,8 +7,16 @@ public class ParticleSpawner : MonoBehaviour
     public bool active; 
     public GameObject particlePrefab;
     private PlatformEffector2D lastEffector;
+    private Vector3 spawnPosition;
+    private Quaternion spawnRotation;
 
     void Start()
+    {
+        spawnPosition = transform.position;
+        spawnRotation = transform.rotation;
+    }
+
+    private void Update()
     {
         
     }
@@ -31,11 +39,29 @@ public class ParticleSpawner : MonoBehaviour
                 }
             }
 
-            Vector3 spawnPosition = new Vector3(collision.contacts[0].point.x, collision.contacts[0].point.y, 0);
-            Quaternion spawnRotation = new Quaternion(collision.contacts[0].normal.x, collision.contacts[0].normal.x, 0, 0);
+            spawnPosition = new Vector3(collision.contacts[0].point.x, collision.contacts[0].point.y, 0);
+            spawnRotation = new Quaternion(collision.contacts[0].normal.x, collision.contacts[0].normal.x, 0, 0);
 
             Instantiate(particlePrefab, spawnPosition, spawnRotation);
         }
        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (active)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Vector3 newSpawnPosition = new Vector3(collision.contacts[0].point.x, collision.contacts[0].point.y, 0);
+                spawnRotation = new Quaternion(collision.contacts[0].normal.x, collision.contacts[0].normal.x, 0, 0);
+                if (Vector3.Distance(newSpawnPosition, spawnPosition) > 1.5f)
+                {
+                    spawnPosition = newSpawnPosition;
+                    Instantiate(particlePrefab, spawnPosition, spawnRotation);
+
+                }
+            }
+        }
     }
 }

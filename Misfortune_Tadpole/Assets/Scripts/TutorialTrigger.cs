df916d47c0b89eced4928d5ecae42926a723f1dd
Axel Eriksson <a.eriksson0303@gmail.com>
2021-01-05 10:58:50 +0100

    Added tutorial text to first level and added tutorial canvas as prefab

---
+++ Misfortune_Tadpole/Assets/Scripts/TutorialTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    private bool tutorialFreeze;
+    private bool shown;
+    public GameObject text;
+    private PauseMenuManager pauseMenuManager;
+    void Start()
+    {
+        pauseMenuManager = GameObject.Find("UI 1").GetComponent<PauseMenuManager>();
+        //text = GameObject.Find("TutorialText");
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && tutorialFreeze)
+        {
+            Time.timeScale = 1f;
+            text.gameObject.SetActive(false);
+            tutorialFreeze = false;
+        } 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !shown)
+        {
+            shown = true;
+            Time.timeScale = 0;
+            text.gameObject.SetActive(true);
+            tutorialFreeze = true;
+        }
+    }
+}
